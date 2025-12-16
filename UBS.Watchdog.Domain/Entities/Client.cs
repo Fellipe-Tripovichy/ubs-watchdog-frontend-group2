fb8a860699fe3ac5cefa582f913bec491aaef50c
GabrielSantana003 <gabcandidosantana@gmail.com>
2025-12-16 16:54:19 -0300

    Added Entities/Client.cs in *.Domain - First Study

---
+++ UBS.Watchdog.Domain/Entities/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// First class created in the project :D
+public class Client
+{
+	//Stable identity as Guid, a few other necessary data for Client with private setters
+	public Guid Id { get; private set; }
+	public string Name { get; private set; }
+	public string Country { get; private set; }
+	public RiskLevel RiskLevel { get; private set; }
+	public KycStatus KycStatus { get; private set; }
+
+	//Creating protected read-only Transactions list
+	public IReadOnlyCollection<Transactions> Transactions => _transactions;
+	private readonly List<Transactions> _transactions = new();
+
+	//Necessary for EF
+	protected Client() { }
+
+	public Client(string name, string country, RiskLevel riskLevel, KycStatus kycStatus)
+	{
+		Id = Guid.NewGuid();
+		Name = name;
+		Country = country;
+		RiskLevel = riskLevel;
+		KycStatus = kycStatus;
+	}
+
+	//Simple domain methods
+	public void GetRiskLevel(RiskLevel newLevel)
+	{
+		RiskLevel = newLevel;
+	}
+
+	public void UpdateKycStatus(KycStatus status)
+	{
+		KycStatus = status;
+	}
+}
