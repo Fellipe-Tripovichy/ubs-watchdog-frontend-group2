61f5fc306b4ea4af75a02ac1bd43718dfcea1adc
= <tthales08@gmail.com>
2025-12-19 22:46:09 -0300

    feat: adding entities

---
+++ UBS.Watchdog.Domain/Entities/Alert.cs
@@ -0,0 +1,46 @@
+namespace UBS.WATCHDOG.Domain.Entities;
+
+using System;
+
+public class Alert
+{
+    public Guid Id { get; private set; }
+    public Guid ClientId { get; private set; }
+    public Guid TransactionId { get; private set; }
+    
+    public string Rule { get; private set; }
+    public AlertStatus Status { get; private set; }
+    public AlertSeverity Severity { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    
+    protected Alert(){ }
+
+    public Alert(Guid clientId, Guid transactionId, string rule, AlertSeverity severity)
+    {
+        Id = Guid.newGuid();
+        ClientId = clientId;
+        TransactionId = transactionId;
+        Rule = rule;
+        Status = AlertStatus.New;
+        Severity = severity;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void StartAnalysis()
+    {
+        if (Status != AlertStatus.New)
+        {
+            throw new InvalidOperation("Apenas novos alertas podem ser analisados...");
+        }
+        Status = AlertStatus.InAnalysis;
+    }
+    
+    public void ResolveAnalysis()
+    {
+        if (Status != AlertStatus.InAnalysis)
+        {
+            throw new InvalidOperationException("Apenas alertas em análise podem ser fechados.");
+        }
+        Status = AlertStatus.Resolved;
+    }
+}
---
+++ UBS.Watchdog.Domain/Entities/Client.cs
@@ -0,0 +1,36 @@
+namespace UBS.WATCHDOG.Domain.Entities;
+
+using System;
+
+public class Client
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Country { get; private set; }
+    public RiskLevel RiskLevel { get; private set; }
+    public KycStatus KycStatus { get; private set; }
+
+    public IReadOnlyCollection<Transaction> Transactions => _transactions;
+    public readonly List<Transaction> Transactions = new List<Transaction>();
+    
+    protected Client(){ }
+
+    public Client(string name, string country, RiskLevel riskLevel, KycStatus kycStatus)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Country = country;
+        RiskLevel = riskLevel;
+        KycStatus = kycStatus;
+    }
+
+    public void UpdateRiskLevel(RiskLevel riskLevel)
+    {
+        RiskLevel = riskLevel;
+    }
+
+    public void UpdateKycStatus(KycStatus kycStatus)
+    {
+        KycStatus = kycStatus;
+    }
+}
