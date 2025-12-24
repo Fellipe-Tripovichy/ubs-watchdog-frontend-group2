c2100a769f0a12772a4199ed49bc010bc5ec6e61
João Rudge <joaorudge465@gmail.com>
2025-12-23 21:25:46 -0300

    feat: add dbcontext and ClienteConfiguration

--- UBS.Watchdog.Domain/Entities/Cliente.cs
+++ UBS.Watchdog.Domain/Entities/Cliente.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Transactions;
 using UBS.Watchdog.Domain.Enums;
 
---
+++ UBS.Watchdog.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UBS.Watchdog.Domain.Entities;
+
+namespace UBS.Watchdog.Infrastructure.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Cliente> Clientes { get; set; } = null!;
+		public DbSet<Alerta> Alertas { get; set; } = null!;
+
+		public DbSet<Transacao> Transacoes { get; set; } = null!;
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+		}
+	}
+}
