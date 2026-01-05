7a44a8f22309a4d79b544a74713b3e56e184385c
GabrielSantana003 <gabcandidosantana@gmail.com>
2026-01-05 00:16:17 -0300

    feat: add clienterepository and interface in infra

---
+++ UBS.Watchdog.Infrastructure/Repositories/Clientes/ClienteRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UBS.Watchdog.Domain.Entities;
+using UBS.Watchdog.Infrastructure.Data;
+
+namespace UBS.Watchdog.Infrastructure.Repositories;
+
+public class ClienteRepository(AppDbContext context) : IClienteRepository
+{
+    public async Task<List<Cliente>> GetAllAsync()
+    {
+        return await context.Clientes.ToListAsync();
+    }
+
+    public async Task<Cliente?> GetByIdAsync(Guid id)
+    {
+        return await context.Clientes.FindAsync(id);
+    }
+
+    public async Task<Cliente> AddAsync(Cliente cliente)
+    {
+        await context.Clientes.AddAsync(cliente);
+        await context.SaveChangesAsync();
+        return cliente;
+    }
+
+    public async Task UpdateAsync(Cliente cliente)
+    {
+        context.Clientes.Update(cliente);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var cliente = await GetByIdAsync(id);
+        context.Clientes.Remove(cliente);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ UBS.Watchdog.Infrastructure/Repositories/Clientes/IClienteRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UBS.Watchdog.Domain.Entities;
+
+namespace UBS.Watchdog.Infrastructure.Repositories;
+
+public interface IClienteRepository
+{
+    Task<List<Cliente>> GetAllAsync();
+
+    Task<Cliente?> GetByIdAsync(Guid id);
+
+    Task<Cliente> AddAsync(Cliente cliente);
+
+    Task UpdateAsync(Cliente cliente);
+
+    Task DeleteAsync(Guid id);
+
+}
