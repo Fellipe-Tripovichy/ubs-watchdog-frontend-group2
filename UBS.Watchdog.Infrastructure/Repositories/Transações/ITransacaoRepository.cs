78e114844d189b516b82a29f23d469be966b4b2d
GabrielSantana003 <gabcandidosantana@gmail.com>
2026-01-05 00:56:57 -0300

    feat: add transacaorepository and interface in infra

---
+++ UBS.Watchdog.Infrastructure/Repositories/Transações/ITransacaoRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UBS.Watchdog.Domain.Entities;
+
+namespace UBS.Watchdog.Infrastructure.Repositories.Transações
+{
+    public interface ITransacaoRepository
+    {
+        Task<List<Transacao>> GetAllAsync();
+
+        Task<Transacao?> GetByIdAsync(Guid id);
+
+        Task<List<Transacao>> GetByPeriodoAsync(DateTime dataInicio, DateTime dataFim);
+
+        Task<List<Transacao>> GetByClienteEPeriodoAsync(Guid clienteId, DateTime dataInicio, DateTime dataFim);
+
+        Task<Transacao> AddAsync(Transacao transacao);
+
+        Task UpdateAsync(Transacao transacao);
+
+        Task DeleteAsync(Guid id);
+
+        Task<List<Transacao>> GetByClienteIdAsync(Guid clienteId);
+
+        //Task<List<Transacao>> GetTransacoesDoDiaAsync(Guid clienteId, DateTime data);
+    }
+}
---
+++ UBS.Watchdog.Infrastructure/Repositories/Transações/TransacaoRepository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UBS.Watchdog.Domain.Entities;
+using UBS.Watchdog.Infrastructure.Data;
+
+namespace UBS.Watchdog.Infrastructure.Repositories.Transações
+{
+    public class TransacaoRepository(AppDbContext context) : ITransacaoRepository
+    {
+        public async Task<Transacao> AddAsync(Transacao transacao)
+        {
+            await context.Transacoes.AddAsync(transacao);
+            await context.SaveChangesAsync();
+            return transacao;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var transacao = await GetByIdAsync(id);
+            context.Transacoes.Remove(transacao);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<Transacao>> GetAllAsync()
+        {
+            return await context.Transacoes.ToListAsync();
+        }
+
+        public async Task<List<Transacao>> GetByClienteIdAsync(Guid clienteId)
+        {
+            return await context.Transacoes
+                .Where(t => t.ClienteId == clienteId)
+                .OrderBy(t => t.DataHora)
+                .ToListAsync();
+        }
+
+        public async Task<Transacao?> GetByIdAsync(Guid id)
+        {
+            return await context.Transacoes.FindAsync(id);
+        }
+
+        public async Task<List<Transacao>> GetByPeriodoAsync(DateTime dataInicio, DateTime dataFim)
+        {
+            return await context.Transacoes
+                .Include(t => t.Cliente)
+                .Where(t => t.DataHora >= dataInicio && t.DataHora <= dataFim)
+                .OrderBy(t => t.DataHora)
+                .ToListAsync();
+        }
+
+        public async Task<List<Transacao>> GetByClienteEPeriodoAsync(Guid clienteId, DateTime dataInicio, DateTime dataFim)
+        {
+            return await context.Transacoes
+                .Where(t => t.ClienteId == clienteId
+                    && t.DataHora >= dataInicio
+                    && t.DataHora <= dataFim)
+                .OrderBy(t => t.DataHora)
+                .ToListAsync();
+        }
+
+        public async Task UpdateAsync(Transacao transacao)
+        {
+            context.Transacoes.Update(transacao);
+            await context.SaveChangesAsync();
+        }
+    }
+}
