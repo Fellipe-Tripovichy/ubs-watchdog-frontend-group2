51af9a41cb8fd6f8024ff2d3f6f5e4d8c916196c
= <tthales08@gmail.com>
2026-01-07 22:49:28 -0300

    adding dto for transacao response

--- UBS.Watchdog.Application/DTOs/Transacao/TransacaoResponse.cs
+++ UBS.Watchdog.Application/DTOs/Transacao/TransacaoResponse.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UBS.Watchdog.Domain.Enums;
 
 namespace UBS.Watchdog.Application.DTOs.Transacao
 {
     public class TransacaoResponse
     {
+        public Guid Id { get; set; }
+        public Guid ClienteId { get; set; }
+        public TipoTransacao Tipo { get; set; }
+        public decimal Valor { get; set; }
+        public Moeda Moeda { get; set; }
+        public string? Contraparte { get; set; }
+        public DateTime DataHora { get; set; }
     }
 }
